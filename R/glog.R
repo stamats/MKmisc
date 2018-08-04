c47306d3a314d045858d556b92c0ab5c72590c9e
Matthias Kohl <Matthias.Kohl@stamats.de>
2018-08-04 18:24:35 +0200

    added functions glog10, glog2; extended vignette

--- R/glog.R
+++ R/glog.R
@@ -2,3 +2,5 @@
 glog <- function(x, base = exp(1)){
   log(x + sqrt(x^2 + 1), base = base) - log(2, base = base)
 }
+glog10 <- function(x){ glog(x, base = 10) }
+glog2 <- function(x){ glog(x, base = 2) }
